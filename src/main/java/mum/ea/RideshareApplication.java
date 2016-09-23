9d5b8d0615e1d1b15e48685030c822a1cdaed991
write2shadi <write2shadi@gmail.com>
2016-09-23 12:23:20 -0500

    new spring tools suit project

---
+++ src/main/java/mum/ea/RideshareApplication.java
@@ -0,0 +1,12 @@
+package mum.ea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideshareApplication.class, args);
+	}
+}
