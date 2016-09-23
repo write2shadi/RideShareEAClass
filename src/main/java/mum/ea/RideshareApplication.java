646a9027c37f60058e96527cc30ae6202bc3ffd4
write2shadi <write2shadi@gmail.com>
2016-09-23 16:45:55 -0500

    added user entity

--- src/main/java/mum/ea/RideshareApplication.java
+++ src/main/java/mum/ea/RideshareApplication.java
@@ -1,14 +1,35 @@
 package mum.ea;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import edu.mum.hw2.control.EntityManager;
+import edu.mum.hw2.control.EntityTransaction;
+import edu.mum.hw2.control.Movie;
+
+
 
 @SpringBootApplication
 public class RideshareApplication {
 
+	private static EntityManagerFactory emf;
+
+	static {
+		try {
+			emf = Persistence.createEntityManagerFactory("RideShareDB");
+		} catch (Throwable ex) {
+			ex.printStackTrace();
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+	
 	public static void main(String[] args) {
-		SpringApplication.run(RideshareApplication.class, args);
-		//commitsss
-		//sooo
+		// SpringApplication.run(RideshareApplication.class, args);
+		
+
+
 	}
 }
---
+++ src/main/java/mum/ea/model/User.java
@@ -0,0 +1,104 @@
+package mum.ea.model;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private int id;
+		
+	private String firstName;
+	private String lastName;
+	private String phone;
+	private String email;
+	private String addressLine1;
+	private String addressLine2;
+	private String city;
+	private String state;
+	private String zipCode;	
+	private String role;
+	private String username;
+	private String password;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
