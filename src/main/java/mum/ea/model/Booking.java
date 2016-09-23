34afb337ce6c451513d8f96a46788ab5953e23bb
stellansh <stellanshuti2@gmail.com>
2016-09-23 18:31:23 -0500

    bookinng

--- src/main/java/mum/ea/model/Booking.java
+++ src/main/java/mum/ea/model/Booking.java
@@ -1,5 +1,29 @@
 package mum.ea.model;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Booking {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	
+	@ManyToMany
+	private User user;
+	
+
+	@ManyToOne
+	private RideInfo ride;
+	
+	private Date timeOfBooking;
+	
+	
 
 }
